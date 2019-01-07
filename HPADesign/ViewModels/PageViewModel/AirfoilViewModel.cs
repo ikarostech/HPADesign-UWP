09667e9afbd56d5a06d7f33cfc956c1aea8ada67
ikarostech <ikarostech@live.jp>
2019-01-07 11:23:18 +0900

    Live Chart Add

---
+++ HPADesign/ViewModels/PageViewModel/AirfoilViewModel.cs
@@ -0,0 +1,44 @@
+﻿using HPADesign.Models;
+using System;
+using Reactive.Bindings;
+using System.IO;
+using Windows.Storage;
+using HPADesign.Services;
+using HPADesign.Utilities;
+using Reactive.Bindings;
+using HPADesign.Helpers;
+using System.Threading.Tasks;
+
+namespace HPADesign.ViewModels
+{
+    class AirfoilViewModel : Observable, IPageViewModel
+    {
+        public Project project { get; }
+
+        public ReadOnlyReactiveCollection<Airfoil> AirfoilList { get; set; }
+        public ReactiveCollection<int> SelectedAirfoilList { get; set; }
+        
+
+        public AirfoilViewModel(Project project)
+        {
+            this.project = project;
+            
+            SelectedAirfoilList = new ReactiveCollection<int>();
+            AirfoilList = project.Airfoils.ToReadOnlyReactiveCollection(x => x);
+        }
+        public async Task AddAirfoil()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.FileTypeFilter.Add(".dat");
+            var files = await picker.PickMultipleFilesAsync();
+            foreach (StorageFile file in files)
+            {
+                Stream stream = await file.OpenStreamForReadAsync();
+                AirfoilReader ar = new AirfoilReader(stream);
+                Airfoil airfoil = ar.Read();
+                project.Airfoils.Add(airfoil);
+            }
+        }
+    }
+    
+}
--- HPADesign/Views/MainPage.xaml.cs
+++ HPADesign/Views/MainPage.xaml.cs
@@ -18,23 +18,18 @@ namespace HPADesign.Views
     {
         
         ConceptViewModel conceptviewmodel { get; }
-        AirfoilViewModel airfoilviewmodel { get; } = new AirfoilViewModel();
+        AirfoilViewModel airfoilviewmodel { get; } 
         AeroDynamicsViewModel aerodynamicsviewmodel { get; }
         Project project { get; set; }
         public MainPage()
         {
             //MainViewModel ViewModel => DataContext as MainViewModel;
             project = new Project();
-            //test
-            Airfoil t1 = new Airfoil();
-            t1.Name = "test1";
-            Airfoil t2 = new Airfoil();
-            t2.Name = "test2";
-            project.Airfoils.Add(t1);
-            project.Airfoils.Add(t2);
+
 
             conceptviewmodel = new ConceptViewModel(project);
             aerodynamicsviewmodel = new AeroDynamicsViewModel(project);
+            airfoilviewmodel = new AirfoilViewModel(project);
             InitializeComponent();
 
             this.DataContext = this;
